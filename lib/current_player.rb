54bbb37530ac76169775f7c98bc4a0f4a57c71b8
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-24 20:32:28 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |move_count|
-    if move_count = "X" || move_count = "O"
+    if move_count == "X" || move_count = "O"
       counter += 1
     end
   end
