728f7d51859152008524df72fe0488a036bd96ba
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-24 20:52:36 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |move_count|
+    if move_count == "X" || move_count == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  #if turn_count(board) % 2 == 0
+  #  current_player = "X"
+  #else
+  #  current_player = "O"
+  #end
+  turn_count(board) % 2 == 0 ?
+  "X" : "O"
+end
